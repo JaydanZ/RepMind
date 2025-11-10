c5db2085ebc7bddf1310d8c4dff482428e93fedd
jzabar <73255741+JaydanZ@users.noreply.github.com>
2025-11-09 22:54:38 -0500

    Init and setup basic main app colors

--- frontend/tailwind.config.mjs
+++ frontend/tailwind.config.mjs
@@ -3,7 +3,17 @@
 export default {
   content: ['./src/**/*.{mjs,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'app-colors': {
+          100: '#A9D9D4',
+          200: '#80BFB4',
+          300: '#80BF6F',
+          400: '#1E2615',
+          500: '#0C0D0A'
+        }
+      }
+    }
   },
   plugins: []
 }
