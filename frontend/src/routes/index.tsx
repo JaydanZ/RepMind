23422c06af55d33f043e3ff982f375928cb61a45
jzabar <73255741+JaydanZ@users.noreply.github.com>
2025-11-15 19:48:36 -0500

    init

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,15 +1,22 @@
+import { useEffect } from 'react'
 import { createFileRoute } from '@tanstack/react-router'
+import axios from 'axios'
 
 export const Route = createFileRoute('/')({
   component: RouteComponent
 })
 
+const backendUrl = import.meta.env.VITE_BACKEND_API_URL
+
 function RouteComponent() {
+  useEffect(() => {
+    const response = axios.get(backendUrl)
+    console.log(response)
+  }, [])
+
   return (
     <div className="flex w-full h-dvh justify-center items-center">
-      <label className="text-app-colors-500 font-bold text-[3rem]">
-        RepMind.
-      </label>
+      <label className="text-white font-bold text-[3rem]">RepMind.</label>
     </div>
   )
 }
