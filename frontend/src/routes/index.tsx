e67d7bc184afdb7c6bdec081ad824a50b8a1ee96
jzabar <73255741+JaydanZ@users.noreply.github.com>
2025-11-24 21:46:55 -0500

    remove unused code

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,20 +1,10 @@
 import { createFileRoute } from '@tanstack/react-router'
-import axios from 'axios'
 
 export const Route = createFileRoute('/')({
   component: RouteComponent
 })
 
-const backendUrl = import.meta.env.VITE_BACKEND_API_URL
-
 function RouteComponent() {
-  const registerBackend = async () => {
-    const response = await axios(backendUrl)
-    console.log(response)
-  }
-
-  registerBackend()
-
   return (
     <div className="flex w-full h-dvh justify-center items-center">
       <label className="text-white font-bold text-[3rem]">RepMind.</label>
