8094d8234d8681eaebc57bccf24f5c2651aefa9d
jzabar <73255741+JaydanZ@users.noreply.github.com>
2025-12-09 22:47:55 -0500

    Implemented pydantic models to format llm response

--- backend/models/programGeneration.py
+++ backend/models/programGeneration.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class ProgramOptions(BaseModel):
     fitness_goal:str
@@ -8,3 +8,23 @@ class ProgramOptions(BaseModel):
     weight:int
     weight_unit:str
     gender:str
+
+
+## List of days working out during the week
+    ## What exercises is the user doing each day (i.e. the body section focus of the day)
+        ## This would also include the ammount of reps and sets per exercise
+        ## Any substitutional exercises
+    ## Any additional tips for the user
+
+class Exercise(BaseModel):
+    name: str = Field(description="Name of the exercise")
+    sets: int = Field(description="Number of sets to perform for the exercise")
+    reps: int = Field(description="Number of reps to perform per set for the exercise")
+class Workout(BaseModel):
+    day: str = Field(description="Name of the day the user will perform the workout (i.e. Monday, Tuesday, etc.)")
+    focus: str = Field(description="Focus of the workout (i.e. Push day, Upper Day, etc.)")
+    exercises: list[Exercise] = Field(description="A collection of exercises the user will perform for the day")
+class ProgramResult(BaseModel):
+    program_frequency: int = Field(description="The number of days the user will workout during the week")
+    program_structure: list[Workout] = Field(description="A collection of workouts the user will perform for the week")
+    program_tips: list[str] = Field(description="A collection of workout tips to help the user along their journey")
--- backend/utils/programGenerator.py
+++ backend/utils/programGenerator.py
@@ -3,9 +3,10 @@ from langchain_core.prompts.chat import (
     HumanMessagePromptTemplate,
     ChatPromptTemplate
 )
+from langchain_core.output_parsers import PydanticOutputParser
 
 from ..config import get_settings
-from ..models.programGeneration import ProgramOptions
+from ..models.programGeneration import ProgramOptions, ProgramResult
 
 ## Get OPEN API key
 envVars = get_settings()
@@ -15,17 +16,20 @@ openai_api_key = envVars.OPENAI_API_KEY
 OPENAI_MODEL = "gpt-5-nano"
 
 open_ai_client = ChatOpenAI(api_key=openai_api_key, model=OPENAI_MODEL)
+parser = PydanticOutputParser(pydantic_object=ProgramResult)
 
 ## Prompt Template
 PROGRAM_GENERATION_PROMPT_TEMPLATE = """
     Assume you're a fitness trainer with over 10 years of experience with weightlifting, bodybuilding, strength training and
     assiting other people to achieve their fitness goals. Please generate a workout program following these arguments:
+    Only schedule workouts on weekdays,
     User's fitness goal: {fitness_goal}
     User's years of experience working out: {years_of_experience}
     User's availability to workout during the week: {days_per_week}
     User's current age: {age}
     User's current weight: {weight} {weight_unit}
     User's gender: {gender}
+    {format_instructions}
 """
 
 def generate_program(program: ProgramOptions):
@@ -36,9 +40,10 @@ def generate_program(program: ProgramOptions):
     ## Insert data from user
     chat_prompt_with_user_data = chat_prompt.format_prompt(fitness_goal=program.fitness_goal, years_of_experience=program.years_of_experience, 
                                                            days_per_week=program.days_per_week, age=program.age, weight=program.weight, 
-                                                           weight_unit=program.weight_unit, gender=program.gender)
+                                                           weight_unit=program.weight_unit, gender=program.gender, format_instructions=parser.get_format_instructions())
     
     response = open_ai_client.invoke(chat_prompt_with_user_data.to_messages())
-    print(response)
+    ##data = parser.parse(response)
+    print(response.content)
 
     return
