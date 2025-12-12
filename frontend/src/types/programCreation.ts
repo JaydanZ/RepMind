7ed036dfa45a2a3c9f9d45e83db848f8154ee8d7
jzabar <73255741+JaydanZ@users.noreply.github.com>
2025-12-11 21:07:57 -0500

    Added types on front-end for handling generation result

--- frontend/src/types/programCreation.ts
+++ frontend/src/types/programCreation.ts
@@ -7,3 +7,21 @@ export interface programOptions {
   weight_unit: string
   gender: string
 }
+
+export interface Exercise {
+  name: string
+  sets: number
+  reps: number
+  exercise_tip: string
+}
+
+export interface Workout {
+  day: string
+  focus: string
+  exercises: Exercise[]
+}
+
+export interface ProgranGenResult {
+  program_structure: Workout[]
+  program_tips_and_goals: string[]
+}
