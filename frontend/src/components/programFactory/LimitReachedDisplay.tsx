c1f6d09e2d93c39c2041e0e7972d790ab82ede36
jzabar <73255741+JaydanZ@users.noreply.github.com>
2026-01-21 00:58:01 -0500

    style update and cleanup for component

--- frontend/src/components/programFactory/LimitReachedDisplay.tsx
+++ frontend/src/components/programFactory/LimitReachedDisplay.tsx
@@ -8,31 +8,33 @@ import { Separator } from '../ui/separator'
 
 export const LimitReachedDisplay = () => {
   return (
-    <div>
-      <Card>
-        <CardHeader className="!p-0 ">
-          <div className="flex flex-col justify-center items-center bg-red-500 w-full h-full rounded-t-xl p-4">
-            <CircleAlert className="!size-12 text-neutral-800 mb-2" />
-            <label className="text-neutral-800 text-2xl font-semibold">
-              Generation Limit Reached
-            </label>
-          </div>
-        </CardHeader>
-        <CardContent className="mt-6 max-w-[600px]">
-          <label className="text-red-500 text-lg font-medium">
-            You have reached the program generation limit for non-logged in
-            users. Please login to continue using this feature!
+    <Card>
+      <CardHeader className="!p-0 ">
+        <div className="flex flex-col justify-center items-center bg-red-500 w-full h-full rounded-t-xl p-4">
+          <CircleAlert className="!size-12 text-neutral-900 mb-2" />
+          <label className="text-neutral-900 text-2xl font-semibold">
+            Generation Limit Reached
           </label>
-        </CardContent>
-        <Separator className="mt-auto mb-6" orientation="horizontal" />
-        <CardFooter className="flex flex-row justify-center">
-          <Link to="/login">
-            <Button variant="secondary" size="lg" className="text-lg px-12">
-              Login
-            </Button>
-          </Link>
-        </CardFooter>
-      </Card>
-    </div>
+        </div>
+      </CardHeader>
+      <CardContent className="mt-6 max-w-[600px]">
+        <label className="text-red-500 text-base font-medium">
+          You have reached the program generation limit for non-logged in users.
+          Please login to continue using this feature!
+        </label>
+      </CardContent>
+      <Separator className="mt-auto mb-6" orientation="horizontal" />
+      <CardFooter className="flex flex-row justify-center">
+        <Link to="/login">
+          <Button
+            variant="secondary"
+            size="lg"
+            className="text-lg px-12 bg-red-500 text-neutral-900 hover:bg-red-700"
+          >
+            Login
+          </Button>
+        </Link>
+      </CardFooter>
+    </Card>
   )
 }
