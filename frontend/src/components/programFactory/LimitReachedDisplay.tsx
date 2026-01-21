68324b0bfd7f9cb4d0c9ac1426de5cd39be6ae04
jzabar <73255741+JaydanZ@users.noreply.github.com>
2026-01-21 00:30:21 -0500

    Implemented design for limit display

--- frontend/src/components/programFactory/LimitReachedDisplay.tsx
+++ frontend/src/components/programFactory/LimitReachedDisplay.tsx
@@ -1,7 +1,38 @@
+import { Link } from '@tanstack/react-router'
+
+import { CircleAlert } from 'lucide-react'
+
+import { Card, CardHeader, CardFooter, CardContent } from '../ui/card'
+import { Button } from '../ui/button'
+import { Separator } from '../ui/separator'
+
 export const LimitReachedDisplay = () => {
   return (
-    <div className="text-red-500 font-bold">
-      This feature can no longer be used. You must login to continue.
+    <div>
+      <Card>
+        <CardHeader className="!p-0 ">
+          <div className="flex flex-col justify-center items-center bg-red-500 w-full h-full rounded-t-xl p-4">
+            <CircleAlert className="!size-12 text-neutral-800 mb-2" />
+            <label className="text-neutral-800 text-2xl font-semibold">
+              Generation Limit Reached
+            </label>
+          </div>
+        </CardHeader>
+        <CardContent className="mt-6 max-w-[600px]">
+          <label className="text-red-500 text-lg font-medium">
+            You have reached the program generation limit for non-logged in
+            users. Please login to continue using this feature!
+          </label>
+        </CardContent>
+        <Separator className="mt-auto mb-6" orientation="horizontal" />
+        <CardFooter className="flex flex-row justify-center">
+          <Link to="/login">
+            <Button variant="secondary" size="lg" className="text-lg px-12">
+              Login
+            </Button>
+          </Link>
+        </CardFooter>
+      </Card>
     </div>
   )
 }
