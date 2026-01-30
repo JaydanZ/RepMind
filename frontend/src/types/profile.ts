6ef956111d7da53157158b7f7d9e21ba037a38e5
jzabar <73255741+JaydanZ@users.noreply.github.com>
2026-01-29 23:48:35 -0500

    Added extra type for workout streak tracker component

--- frontend/src/types/profile.ts
+++ frontend/src/types/profile.ts
@@ -4,11 +4,18 @@ export interface Profile {
   username: string
   email: string
   programs: Program[] | null
-  activeProgram: Program | null
+  active_program: Program | null
+  workout_streak_tracker: WorkoutTracker[] | null
+}
+
+export interface WorkoutTracker {
+  has_worked_out: boolean
+  program_link: string | null
+  date: string | null
 }
 
 export interface Program {
-  isActiveProgram: boolean
+  is_active_program: boolean
   program_structure: Workout[]
   last_updated: string
 }
