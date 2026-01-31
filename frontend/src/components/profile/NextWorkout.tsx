e3ad02ac15eabd0105937213dde68e7cc5243665
jzabar <73255741+JaydanZ@users.noreply.github.com>
2026-01-30 23:46:12 -0500

    Added base component files for profile page

---
+++ frontend/src/components/profile/NextWorkout.tsx
@@ -0,0 +1,3 @@
+export default function NextWorkout() {
+  return <div>NextWorkout</div>
+}
---
+++ frontend/src/components/profile/WorkoutStreakTracker.tsx
@@ -0,0 +1,3 @@
+export default function WorkoutStreakTracker() {
+  return <div>WorkoutStreakTracker</div>
+}
