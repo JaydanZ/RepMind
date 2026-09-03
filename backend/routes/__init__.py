b09146166ec257986f69b2b1c182f270bf7af924
jzabar <73255741+JaydanZ@users.noreply.github.com>
2026-09-02 23:58:00 -0400

    updated naming of programs generation route to programs for future updates

--- backend/routes/__init__.py
+++ backend/routes/__init__.py
@@ -1,5 +1,5 @@
 from .auth import auth_router
 from .profile import profile_router
-from .programGeneration import program_generation_router
+from .programs import programs_router
 
 print("Imported routes")
--- backend/routes/programs.py
+++ backend/routes/programs.py
@@ -4,15 +4,16 @@ from ..utils.programGenerator import generate_program
 
 
 ## Endpoint to handle ai program generation without authentication / is user logged in status
-program_generation_router = APIRouter(
+programs_router = APIRouter(
     prefix="/programs",
     tags=["programs"]
 )
 
-@program_generation_router.post('/', status_code=201)
+@programs_router.post('/', status_code=201)
 def handleProgramGeneration(programInput: ProgramOptions):
     ## Check if free limit is enabled -> means user is not logged in
     if(programInput.freeLimitEnabled == True):
         raise HTTPException(status_code=401, detail="User must login to continue using API")
     content = generate_program(programInput)
     return content
+
